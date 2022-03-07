8c412bdaa8a7df212d0b5f085f3d0c7aa1b9f82e
Habibur Rahman <habiburrahman0540@gmail.com>
2022-03-07 20:43:51 +0600

    Design Navbar and footer

---
+++ src/Pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <h1 className='text-6xl mb-4'>Github Finder</h1>
+        <p className='mb-4 text-2xl font-light'>A react app to search GitHub profiles and see profile details on GitHUB </p>
+        <p className='text-lg text-gray-400' >version ,<span className='text-gray-800'>1.0.1</span></p>
+        <p>Layout Design By <a href="https://www.fiverr.com/share/ad3KG7">Habibur Rahman</a></p>
+    </div>
+  )
+}
+
+export default About
